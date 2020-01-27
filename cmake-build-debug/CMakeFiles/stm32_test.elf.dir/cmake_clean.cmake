cf50911dfc32b5643f1e0d1b71b0f1bdf0afb66d
chilogen <chilogen@self.com>
2020-01-27 01:56:16 -0600

    feat: DMA

--- cmake-build-debug/CMakeFiles/stm32_test.elf.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/stm32_test.elf.dir/cmake_clean.cmake
@@ -12,6 +12,7 @@ file(REMOVE_RECURSE
   "CMakeFiles/stm32_test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj"
   "CMakeFiles/stm32_test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj"
   "CMakeFiles/stm32_test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj"
+  "CMakeFiles/stm32_test.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj"
   "CMakeFiles/stm32_test.elf.dir/Src/main.c.obj"
   "CMakeFiles/stm32_test.elf.dir/Src/stm32f1xx_hal_msp.c.obj"
   "CMakeFiles/stm32_test.elf.dir/Src/stm32f1xx_it.c.obj"
